2809205b8d1a5cef15ddaffed5af787ed5672133
Afsana Mammadova 16063 <amammadova16063@ada.edu.az>
2024-01-01 20:44:09 +0400

    before moving to the styling i wanted to see all info in the screen, such as question, options, question, options and etc. Right now it is just a simple text, now it will design it as cards

--- src/FlashCard.js
+++ src/FlashCard.js
@@ -2,9 +2,25 @@ import React, { useState } from "react";
 
 export default function FlashCard({ flashcard }) {
   const [flip, setFlip] = useState(false);
+
   return (
-    <div onClick={() => setFlip(!flip)}>
-      {flip ? flashcard.answer : flashcard.questions}
+    <div
+      className={`card ${flip ? "flip" : ""}`}
+      onClick={() => setFlip(!flip)}
+    >
+      <div className="flashcard-questions">
+        {flashcard.questions}
+        <div className="flashcard-options">
+          {flashcard.options.map((option) => {
+            return <div className="flashcard-option">{option}</div>;
+          })}
+        </div>
+      </div>
+      <div className="flashcard-answers">{flashcard.answers}</div>
     </div>
+
+    // <div onClick={() => setFlip(!flip)}>
+    //   {flip ? flashcard.answer : flashcard.questions}
+    // </div>
   );
 }
