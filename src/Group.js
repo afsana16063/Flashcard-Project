d7a20059829b9ea8099b828427f65b13278d2091
Afsana Mammadova 16063 <amammadova16063@ada.edu.az>
2024-01-01 20:25:21 +0400

    had a small issue now i fixed it

--- src/Group.js
+++ src/Group.js
@@ -2,10 +2,11 @@ import React from "react";
 import FlashCard from "./FlashCard";
 
 export default function Group({ flashcards }) {
-  return;
-  <div className="card-grid">
-    {flashcards.map((flashcard) => {
-      return <FlashCard flashcard={flashcard} key={flashcard.id} />;
-    })}
-  </div>;
+  return (
+    <div className="card-grid">
+      {flashcards.map((flashcard) => {
+        return <FlashCard flashcard={flashcard} key={flashcard.id} />;
+      })}
+    </div>
+  );
 }
